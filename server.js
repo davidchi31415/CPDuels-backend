0505fbe3c843d33252af1df1bda76511091dedfa
David Chi <davidchi@tamu.edu>
2022-09-20 22:21:04 -0500

    First working version of websockets.

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import cors from 'cors';
 import duelsRouter from './routes/duelsRouter.js';
 import problemsRouter from './routes/problemsRouter.js';
 import update_problemset from './utils/tasks.js';
-import * as http from 'http';
-import { Server } from 'socket.io'
+import { Server } from 'socket.io';
 
 // ENVIRONMENT VARIABLES
 import dotenv from 'dotenv';
@@ -13,20 +12,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/duel', duelsRouter);
-app.use('/problem', problemsRouter);
-
-const httpServer = http.createServer(app);
-
-const options = {
-    cors: {
-        origin: `http://localhost:${process.env.FRONTEND_PORT}`,
-        methods: ["GET", "POST"]
-    }
-};
-const io = new Server(httpServer, options);
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
@@ -34,21 +19,31 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 db.once('open', () => console.log("Connected to database."));
 
-// update_problemset();
+update_problemset();
 
-httpServer.listen(process.env.BACKEND_PORT, () => console.log("Server is started."));
+app.use(cors({ origin: `http://localhost:${process.env.FRONTEND_PORT}` }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/duel', duelsRouter);
+app.use('/problem', problemsRouter);
 
+app.listen(process.env.BACKEND_PORT, () => console.log("Server is started."));
+
+const io = new Server(8081, { cors: { origin: "http://localhost:3000" } });
 const timeouts = [];
 
-io.on('Start Timer', () => {
-    let timeout = setTimeout(() => {
-        io.emit('Duel Over');
-    }, 5000);
-    timeouts.push(timeout);
-});
-io.on('Stop Timer', () => {
-    clearTimeout(timeouts[timeouts.length-1]);
-    console.log("Timer stopped.");  
+io.on('connection', (socket) => {
+    socket.on('Start Timer', () => {
+        console.log('Yo here we go again');
+        let timeout = setTimeout(() => {
+            io.emit('Duel Over');
+        }, 5000);
+        timeouts.push(timeout);
+    });
+    socket.on('Stop Timer', () => {
+        clearTimeout(timeouts[timeouts.length-1]);
+        console.log("Timer stopped.");  
+    });
 });
 
 export default db;
