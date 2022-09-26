182f9b160776bca30d1621e6f8d476d748ad0ac9
JelBell1324 <jeffthegreat66@gmail.com>
2022-09-26 18:46:13 -0500

    http listen

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/duel', duelsRouter);
 app.use('/problem', problemsRouter);
 
-app.listen(PORT, () => console.log("Server is started."));
+http.listen(PORT, () => console.log("Server is started."));
 
 export default db;
