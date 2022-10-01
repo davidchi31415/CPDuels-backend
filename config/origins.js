708eea9f8c9bbc0182d71b87b7abedfafee73deb
David Chi <davidchi@tamu.edu>
2022-09-30 20:58:58 -0500

    added back duel request validation and updated cors to have one origin

--- config/origins.js
+++ config/origins.js
@@ -1,5 +1,5 @@
 import DEBUG from './debug.js';
 
-const allowedOrigins = DEBUG ? ['http://localhost:3000'] : ['https://cpduels.onrender.com:*', 'https://www.cpduels.com:*'];
+const allowedOrigins = DEBUG ? ['http://localhost:3000'] : ['https://www.cpduels.com:*'];
 
 export default allowedOrigins;
