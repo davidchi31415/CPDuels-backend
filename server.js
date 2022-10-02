4b72bfd6ec01482addd9a09261cc9f25cd930778
JelBell1324 <jeffthegreat66@gmail.com>
2022-10-02 05:25:41 -0500

    cors

--- server.js
+++ server.js
@@ -29,23 +29,20 @@ while(mongoose.connection.readyState != 1) {
 
 app.use(cors());
 app.options('*', cors());
+
+
+
+app.options('*', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send('ok');
+  });
+  
+  app.use((req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+  });
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-app.use((req, res, next) => { //doesn't send response just adjusts it
-    res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
-    );
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
-        return res.status(200).json({});
-    }
-    next(); //so that other routes can take over
-})
-
 app.use('/duels', duelsRouter);
 app.use('/cfproblems', cfproblemsRouter);
 
