c3771bb70bd39f14ba2756ebc36f3139f98c15de
JelBell1324 <jeffthegreat66@gmail.com>
2022-10-10 00:35:00 -0500

    added try/catch and functionality

--- utils/submissionsManager.js
+++ utils/submissionsManager.js
@@ -5,53 +5,53 @@ class SubmissionManager {
 
     static async findCsrf(body) {
         let re = /(?<="X-Csrf-Token" content=")((.*?)(?=")|(?="))/gs;
-        console.log("findCsrf called " + body.match(re)[0]);
         return body.match(re)[0];
     }
 
     static async login() {
-        let resp = await client.get('https://codeforces.com/enter/');
-        console.log(resp.status);
-    
-        let csrf = findCsrf(resp.text);
-        await client
-            .post('http://codeforces.com/enter/')
-            .send({
-                _tta: "176",
-                csrf_token: csrf,
-                action: "enter",
-                handleOrEmail: "",
-                password: "",
-            })
-            .set('Content-Type','application/x-www-form-urlencoded')
-            .then(res => {
-                console.log(res.status);
-            })
+        try {
+            let resp = await client.get('https://codeforces.com/enter/');        
+            let csrf = await this.findCsrf(resp.text);
+            await client
+                .post('http://codeforces.com/enter/')
+                .send({
+                    _tta: "176",
+                    csrf_token: csrf,
+                    action: "enter",
+                    handleOrEmail: "",
+                    password: "",
+                })
+                .set('Content-Type','application/x-www-form-urlencoded')
+                .then(res => {console.log(`Login Succeeded ${res.status}`)});
+        } catch (err) {
+            console.log(`Login failed: \n ${err}`);
+        }
     }
     
     
     static async submit(contestId,problemIndex,sourceCode) {
-        let resp = await client.get(`https://codeforces.com/contest/${contestId}/submit`); 
-        console.log(resp.status);
-        let csrf = await findCsrf(resp.text);
-        let submitUrl = `https://codeforces.com/contest/${contestId}/submit?csrf_token=${csrf}`;
-        await client
-            .post(submitUrl)
-            .send({
-                csrf_token:             csrf,
-                action:                 "submitSolutionFormSubmitted",
-                submittedProblemIndex:  problemIndex,
-                programTypeId:          "73",
-                contestId:              contestId,
-                source:                 sourceCode,
-                tabSize:                "4",
-                _tta:                   "594",
-                sourceCodeConfirmed:    "true",
-            })
-            .set('Content-Type','application/x-www-form-urlencoded')
-            .then(res => {
-                console.log(res.status);
-            })
+        try {
+            let resp = await client.get(`https://codeforces.com/contest/${contestId}/submit`);
+            let csrf = await this.findCsrf(resp.text);
+            let submitUrl = `https://codeforces.com/contest/${contestId}/submit?csrf_token=${csrf}`;
+            await client
+                .post(submitUrl)
+                .send({
+                    csrf_token:             csrf,
+                    action:                 "submitSolutionFormSubmitted",
+                    submittedProblemIndex:  problemIndex,
+                    programTypeId:          "73",
+                    contestId:              contestId,
+                    source:                 sourceCode,
+                    tabSize:                "4",
+                    _tta:                   "594",
+                    sourceCodeConfirmed:    "true",
+                })
+                .set('Content-Type','application/x-www-form-urlencoded')
+                .then(console.log(`Submitted solution for ${contestId}${problemIndex}`));
+        } catch (err) {
+            console.log(`Submitting solution for ${contestId}${problemIndex} Failed: \n ${err}`);
+        }
     }
 
 }
