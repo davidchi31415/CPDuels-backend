ba92d5e65fcbcaad563a11cec740f37f2e24bf27
David Chi <davidchi@tamu.edu>
2022-12-05 22:59:49 -0600

    Turned checker back on

--- managers/SocketManager.js
+++ managers/SocketManager.js
@@ -16,21 +16,21 @@ class SocketManager {
     );
     this.timers = {};
     // taskManager.init();
-    // setInterval(async () => {
-    //   let updatedCFSubmissions = await this.codeforcesAPI.updateSubmissions();
-    //   if (updatedCFSubmissions?.length) {
-    //     for (const item of updatedCFSubmissions) {
-    //       await duelManager.updateProblem(
-    //         item.duelId,
-    //         item.uid,
-    //         item.problemNumber,
-    //         item.status,
-    //         item.createdAt
-    //       );
-    //       io.emit("submission-change", { duelId: item.duelId });
-    //     }
-    //   }
-    // }, 10000);
+    setInterval(async () => {
+      let updatedCFSubmissions = await this.codeforcesAPI.updateSubmissions();
+      if (updatedCFSubmissions?.length) {
+        for (const item of updatedCFSubmissions) {
+          await duelManager.updateProblem(
+            item.duelId,
+            item.uid,
+            item.problemNumber,
+            item.status,
+            item.createdAt
+          );
+          io.emit("submission-change", { duelId: item.duelId });
+        }
+      }
+    }, 10000);
     this.io = io;
     io.on("connection", async (socket) => {
       socket.on("join", (roomId) => {
